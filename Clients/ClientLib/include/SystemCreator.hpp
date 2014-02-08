a3875d043aee221975ae840eb434d5b9d8ca8aa9
jellysheep <max.mail@dameweb.de>
2014-02-08 08:40:17 +0100

    Updated license information and fixed mingw-w64 PKGBUILD.

--- Clients/ClientLib/include/SystemCreator.hpp
+++ Clients/ClientLib/include/SystemCreator.hpp
@@ -1,7 +1,7 @@
 /** \file
  * \author Max Mertens <max.mail@dameweb.de>
  * \section LICENSE
- * Copyright (c) 2013, Max Mertens.
+ * Copyright (c) 2014, Max Mertens.
  * All rights reserved.
  * This file is licensed under the "BSD 3-Clause License".
  * Full license text is under the file "LICENSE" provided with this code. */
